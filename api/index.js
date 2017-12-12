09939e2ba1ec88c07d6f096a47d699b74a75ce3b
Ainul <ainul.y9b@gmail.com>
2017-12-12 18:47:14 +0700

    connecting database to a contests

--- api/index.js
+++ api/index.js
@@ -1,27 +1,50 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config from '../config';
 // same as data.contest
-import { contest } from '../src/testData.json';
+// import { contest } from '../src/testData.json';
 
 const Router = express.Router();
-const data = contest.reduce((obj, contest) => {
-                obj[contest.id] = contest;
-                return obj;
-            }, {});
+// const data = contest.reduce((obj, contest) => {
+//                 obj[contest.id] = contest;
+//                 return obj;
+//             }, {});
 
 
-// Every route here, have a /api before the route
-// Returning the json array object
+var mdb;
+MongoClient.connect(config.mongodbUri, (err, db) => {
+    assert.equal(null, err)
+
+    mdb = db;
+})
+
 Router.get('/contest', (req, res) => {
-    res.json({
-        data: data
-    });
+    let contests = {};
+
+    mdb.collection('contests').find({})
+        .project({
+            id: 1,
+            categoryName: 1,
+            contestName: 1
+        })
+        .each((err, contest) => {
+            assert.equal(null, err);
+
+            if (!contest) {
+                res.send({data: contests});
+                return
+            }
+
+            contests[contest.id] = contest
+        })
 });
 
 Router.get('/contest/:contestId', (req, res) => {
-    let contests = data[req.params.contestId];
-    contests.description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolore iure aspernatur veritatis vel, et aut unde quae eveniet, asperiores dolorum nam beatae tempore numquam sit fugiat ad recusandae aperiam!'
-    
-    res.send(contests);
+    mdb.collection('contests')
+        .findOne({ id: parseInt(req.params.contestId) })
+        .then(contest => res.send(contest))
+        .catch(err => console.log(err))
 });
 
 export default Router;
